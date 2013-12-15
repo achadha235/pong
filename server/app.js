5f9a8d6861a3d38e54d2306f8cb80fcaccb5d4b1
Abhishek Chadha <achadha@andrew.cmu.edu>
2013-12-14 19:24:38 -0500

    more

--- server/app.js
+++ server/app.js
@@ -18,26 +18,57 @@ function handler (req, res) {
 
 
 var model = {
-	p1: 0,
-	p2: 0,
-	p3: 0,
-	p4: 0,
-	ball: {x: 200, y: 200}
+	positions: [100, 100, 100, 100],
+	actions: [0, 0, 0, 0],
+	ball: {x: 200, y: 200, dx: 0, dy: 0}
 }
+
 var numPlayers = 0;
 var players = [];
 var idHash = {};
+var velocity = 10;
 
 io.sockets.on('connection', function (socket) {
-	if (numPlayers <= 4){ // Add Player
+	if (numPlayers < 4){ // Add Player
 		socket.emit('init', {test: "Hello World!"});
-		console.log(socket.id);
-		players.push(socket)\
+		idHash[socket.id] = numPlayers;
+		players.push(socket);
 		numPlayers++;
 	}
 
 	socket.on('moveLeft', function (){
-
+		model.actions[idHash[socket.id]] = 1;
+		io.sockets.emit('update', model);
 	})
-
+	socket.on('moveRight', function (){
+		model.actions[idHash[socket.id]] = 2;
+		io.sockets.emit('update', model);
+	})
+	socket.on('stop', function (){
+		model.actions[idHash[socket.id]] = 0;
+		io.sockets.emit('update', model);
+	})
+	startGame()
 });
+
+function startGame(){
+	var step = 10;
+
+	setTimeout(function (){
+		// Network Update loop 
+		io.sockets.emit('update', model)
+	}, 30)
+
+	setTimeout(function (){
+		// Game Update loop 
+		for (var i = 0; i < 4; i++){
+			if (model.actions[i] === 1){
+				model.positions[i] = Math.max(0, model.positions[i] - (velocity * (step/1000)))
+			}
+			else if (model.actions[i] === 2){
+				model.positions[i] = Math.min(400, model.positions[i] + (velocity * (step/1000)))
+			}
+		}
+	}, 10)
+
+}
